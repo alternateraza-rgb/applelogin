0023637c69d877b41d4f0d3740602e886478fc45
alternateraza-rgb <alternateraza@gmail.com>
2026-03-13 04:38:52 +0800

    fixed icloud image

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,7 @@ export default function Main(){
     return(
         <div className='main'>    
             <div className='main-items'>    
-                <img className="item-1" src="https://www.icloud.com/system/icloud.com/2413Project46/en-us/4f72d89d71e9abcc4e37c71fb77fe65b.svg" alt="logo"/>
-<img className="item-1" src="/icloud-logo.png" alt="iCloud logo" />
+                <img className="item-1" src="/icloud-logo.png" alt="iCloud logo" />
                 <h1 className="item-2">Sign in with Apple ID</h1>
         
                 <input type="text" id="account_name_text_field" can-field="accountName" autocomplete="off" autocorrect="off" autocapitalize="off" aria-required="true" required="required" spellcheck="false" class="force-ltr form-textbox form-textbox-text " placeholder="Apple ID" aria-invalid="false" autofocus="" className='item-3'/>
