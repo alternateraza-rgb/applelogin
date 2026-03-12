67b5e2c665a92f31ec30adfce1525305f7a0c61f
alternateraza-rgb <alternateraza@gmail.com>
2026-03-13 04:33:38 +0800

    added apple logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(){
     return(
         <div className="navbar">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Apple_logo_dark_grey.svg/800px-Apple_logo_dark_grey.svg.png" alt="logo"/>
+            <img src="/apple-logo.svg" alt="Apple logo" />
             <h1>iCloud</h1>
         </div>
     )
